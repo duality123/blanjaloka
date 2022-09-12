9b7a9f9115414476bd74d37fc127d2eeabf0ad73
rickydamarsaputra <retrocode.rc@gmail.com>
2022-09-12 08:21:04 +0700

    menambahkan oauth google

--- routes/web.php
+++ routes/web.php
@@ -32,19 +32,24 @@ Route::get('/reset_password',[AuthController::class,'resetPasswordPage'])->name(
 Route::get('/aktivasi/{token}',[AuthController::class,'tokenLinkVerify'])->name('tokenLinkVerify');
 Route::post('/aktivasi',[AuthController::class,'tokenInputVerify'])->name('tokenInputVerify');
 Route::post('/proses_konfirmasi',[AuthController::class,'proses_konfirmasi'])->name('konfirmasi');
-*/
+ */
 Route::post('/email/verification-notification', function (Request $request) {
     $request->user()->sendEmailVerificationNotification();
- 
+
     return back()->with('message', 'Verification link sent!');
 })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
     $request->fulfill();
- 
+
     return redirect('/authenticated_page');
 })->middleware(['auth', 'signed'])->name('verification.verify');
 
 
 
-Route::get('/authenticated_page',[AuthenticatedController::class,'auth_page'])->middleware(['auth','verified'])->name('authenticated_page');
+Route::get('/authenticated_page', [AuthenticatedController::class, 'auth_page'])->middleware(['auth', 'verified'])->name('authenticated_page');
+
+Route::controller(AuthController::class)->prefix('auth')->name('auth.')->group(function () {
+    Route::get('/google/redirect', 'oauthGoogleRedirect')->name('google.redirect');
+    Route::get('/google/callback', 'oauthGoogleCallback')->name('google.callback');
+});
