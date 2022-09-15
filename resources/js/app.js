00d051a286a50aaf70fff414bf1f401038b80db7
rickydamarsaputra <retrocode.rc@gmail.com>
2022-09-15 12:04:36 +0700

    setup bootstrap vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,40 +1,40 @@
-import './bootstrap';
-import '../css/app.css'; 
+import { createApp, h } from "vue";
+import { createInertiaApp } from "@inertiajs/inertia-vue3";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { InertiaProgress } from "@inertiajs/progress";
+import "bootstrap";
 
-import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/inertia-vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { InertiaProgress } from '@inertiajs/progress'
- 
 createInertiaApp({
-  resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
-  setup({ el, App, props, plugin }) {
-    createApp({ render: () => h(App, props) })
-    //set mixins
-    // .mixin({
-    //     methods: {
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob("./Pages/**/*.vue")
+        ),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            //set mixins
+            // .mixin({
+            //     methods: {
 
-    //       //method "hasAnyPermission"
-    //       hasAnyPermission: function (permissions) {
+            //       //method "hasAnyPermission"
+            //       hasAnyPermission: function (permissions) {
 
-    //         //get permissions from props
-    //         let allPermissions = this.$page.props.auth.permissions;
+            //         //get permissions from props
+            //         let allPermissions = this.$page.props.auth.permissions;
 
-    //         let hasPermission = false;
-    //         permissions.forEach(function(item){
-    //           if(allPermissions[item]) hasPermission = true;     
-    //         });
+            //         let hasPermission = false;
+            //         permissions.forEach(function(item){
+            //           if(allPermissions[item]) hasPermission = true;
+            //         });
 
-    //         return hasPermission;
-    //       }
+            //         return hasPermission;
+            //       }
 
-    //     },
-    //   })
-      .use(plugin)
-      .mount(el)
-  },
+            //     },
+            //   })
+            .use(plugin)
+            .mount(el);
+    },
 });
 
-InertiaProgress.init()
-
-
+InertiaProgress.init();
