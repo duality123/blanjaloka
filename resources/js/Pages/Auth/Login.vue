5e36669b7b1fb58fba657c87d4c37cd6f93a67f8
Maman <novita.vsalama@gmail.com>
2022-09-18 22:36:23 +0700

    remake ui login & register

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,133 +1,304 @@
 <template>
-
     <Head>
         <title>Login Blanjaloka</title>
     </Head>
-    <div class="col-md-4">
-        <div class="fade-in">
-            <div class="text-center mb-4">
-                <a href="" class="text-dark text-decoration-none">
-                    <h3 class="mt-2 font-weight-bold">Blanjaloka</h3>
-                </a>
+    <div class="form-login">
+        <div class="d-flex justify-content-evenly">
+            <div class="col-lg-6 d-lg-block d-none">
+                <img
+                    class="hero-images"
+                    src="/images/hero.png"
+                    alt="hero images"
+                />
             </div>
-            <div class="card-group">
-                <div class="card border-top-purple border-0 shadow-sm rounded-3">
-                    <div class="card-body">
-                        <div class="text-start">
-                            <h5>LOGIN ACCOUNT</h5>
-                            <p class="text-muted">Sign In to your account</p>
-                        </div>
-                        <hr>
-                        <div v-if="session.status" class="alert alert-success mt-2">
-                            {{ session.status }}
-                        </div>
-                        <form @submit.prevent="submit">
-                            <div class="input-group mb-3">
-                               
-                                <input class="form-control" v-model="form.email" :class="{ 'is-invalid': errors.email }" type="email" placeholder="Email Address">
-                            </div>
-                            <div v-if="errors.email" class="alert alert-danger">
-                                {{ errors.email }}
-                            </div>
-                            <div class="input-group mb-3">
-                                
-                                <input class="form-control" v-model="form.password" :class="{ 'is-invalid': errors.password }" type="password" placeholder="Password">
-                            </div>
-                            <div v-if="errors.password" class="alert alert-danger">
-                                {{ errors.password }}
-                            </div>
-                            <div class="row">
-                                <div class="col-12 mb-3 text-end">
-                                    <Link href="/forgot-password">Forgot Password?</Link>
-                                </div>
-                                <div class="col-12">
-                                    <button class="btn btn-primary shadow-sm rounded-sm px-4 w-100" type="submit">LOGIN</button>
-                                </div>
-                            </div>
-                             <div class="mb-3">
-                            <a href="auth/google/redirect" class="btn btn-danger d-grid w-100">
-                                <div>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                        class="bi bi-google" viewBox="0 0 16 16">
-                                        <path
-                                            d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
-                                    </svg>
-                                    Sign in with google
-                                </div>
-                            </a>
-                        </div>
-                        </form>
-                    </div>
+            <div class="col-12 col-lg-4 col-md-8 p-4">
+                <div class="logo-login mb-default">
+                    <img src="/images/logo.png" alt="logo" />
                 </div>
+                <h3 class="text-4xl fw-semibold">Masuk</h3>
+                <p class="text-secondary mb-4 fs-6">
+                    Ayo lakukan pendanaan UMKM sekarang juga!
+                </p>
+                <div v-if="session.status" class="alert alert-success mt-2">
+                    {{ session.status }}
+                </div>
+                <form @submit.prevent="submit">
+                    <div class="mb-4">
+                        <label
+                            class="fw-semibold"
+                            for="exampleFormControlInput1"
+                            >Email</label
+                        >
+                        <input
+                            type="email"
+                            class="form-control border-bottom border-0 rounded-0 px-0 text-sm"
+                            v-model="form.email"
+                            id="exampleFormControlInput1"
+                            :class="{ 'is-invalid': errors.email }"
+                            placeholder="Masukkan email Anda"
+                        />
+                    </div>
+                    <div v-if="errors.email" class="alert alert-danger">
+                        {{ errors.email }}
+                    </div>
+                    <div class="mb-3">
+                        <label
+                            class="fw-semibold"
+                            for="exampleFormControlInput1"
+                            >Kata Sandi</label
+                        >
+                        <input
+                            type="password"
+                            class="form-control border-bottom border-0 rounded-0 px-0 text-sm"
+                            id="exampleFormControlInput1"
+                            v-model="form.password"
+                            :class="{ 'is-invalid': errors.password }"
+                            placeholder="Masukkan kata sandi Anda"
+                        />
+                    </div>
+                    <div v-if="errors.password" class="alert alert-danger">
+                        {{ errors.password }}
+                    </div>
+                    <div
+                        class="d-flex align-items-center justify-content-between"
+                    >
+                        <div class="form-check">
+                            <input
+                                class="form-check-input scale-1"
+                                type="checkbox"
+                                value=""
+                                id="flexCheckDefault"
+                            />
+                            <label
+                                class="form-check-label text-sm"
+                                for="flexCheckDefault"
+                            >
+                                Ingat Saya
+                            </label>
+                        </div>
+                        <a class="text-sm" href="#">Lupa Kata Sandi?</a>
+                    </div>
+                    <div class="mt-default mb-3">
+                        <button
+                            type="submit"
+                            class="btn btn-default col-12 btn-primary"
+                        >
+                            Masuk
+                        </button>
+                    </div>
+                    <div
+                        class="d-flex mb-3 align-items-center gap-2 justify-content-center"
+                    >
+                        <p class="text-sm mb-0">Belum punya akun?</p>
+                        <a class="text-sm" href="/register">Daftar Sekarang</a>
+                    </div>
+                    <div class="line-center position-relative text-center">
+                        <span
+                            class="line bg-default text-center text-sm text-gray-100 px-3"
+                            >atau masuk dengan</span
+                        >
+                    </div>
+                    <div
+                        class="social-media mt-4 d-flex gap-3 align-items-center justify-content-center"
+                    >
+                        <a href="">
+                            <img
+                                class="icon-facebook"
+                                src="/images/icon-facebook.png"
+                                alt="facebook"
+                            />
+                        </a>
+                        <a href="auth/google/redirect">
+                            <img
+                                class="icon-google"
+                                src="/images/icon-google.png"
+                                alt=""
+                            />
+                        </a>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    //import layout
-    import LayoutAuth from '../../Layouts/Auth.vue';
+//import layout
+import LayoutAuth from "../../Layouts/Auth.vue";
 
-    //import reactive
-    import { reactive } from 'vue';
+//import reactive
+import { reactive } from "vue";
 
-    //import inertia adapter
-    import { Inertia } from '@inertiajs/inertia';
+//import inertia adapter
+import { Inertia } from "@inertiajs/inertia";
 
-    //import Heade and useForm from Inertia
-    import {
+//import Heade and useForm from Inertia
+import { Head, Link } from "@inertiajs/inertia-vue3";
+
+export default {
+    //layout
+    layout: LayoutAuth,
+
+    //register component
+    components: {
         Head,
         Link,
-    } from '@inertiajs/inertia-vue3';
+    },
 
-    export default {
+    props: {
+        errors: Object,
+        session: Object,
+    },
 
-        //layout
-        layout: LayoutAuth,
+    //define composition API
+    setup() {
+        //define form state
+        const form = reactive({
+            email: "",
+            password: "",
+        });
 
-        //register component
-        components: {
-            Head,
-            Link
-        },
-
-        props: {
-            errors: Object,
-            session: Object
-        },
-
-        //define composition API
-        setup() {
-
-            //define form state
-            const form = reactive({
-                email: '',
-                password: '',
+        //submit method
+        const submit = () => {
+            //send data to server
+            Inertia.post("/login", {
+                //data
+                email: form.email,
+                password: form.password,
             });
+        };
 
-            //submit method
-            const submit = () => {
-
-                //send data to server
-                Inertia.post('/login', {
-
-                    //data
-                    email: form.email,
-                    password: form.password,
-                });
-            }
-
-            //return form state and submit method
-            return {
-                form,
-                submit,
-            };
-
-        }
-
-    }
+        //return form state and submit method
+        return {
+            form,
+            submit,
+        };
+    },
+};
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap");
+body {
+    font-family: "Inter", sans-serif;
+}
 
+.logo-login {
+    cursor: pointer;
+    width: 120px;
+}
+
+img {
+    width: 100%;
+}
+
+.icon-facebook,
+.icon-google {
+    cursor: pointer;
+    width: 45px;
+}
+
+.text-sm {
+    font-size: 0.9rem !important;
+}
+
+.text-base {
+    font-size: 1rem;
+}
+
+.text-4xl {
+    font-size: 1.4rem;
+}
+
+.scale-1 {
+    transform: scale(0.8);
+}
+
+textarea:hover,
+input:hover,
+textarea:active,
+input:active,
+textarea:focus,
+input:focus,
+button:focus,
+button:active,
+button:hover,
+label:focus,
+.btn:active,
+.btn.active {
+    outline: 0px !important;
+    box-shadow: none !important;
+}
+
+.btn-default {
+    padding: 0.6rem 0.5rem !important;
+    opacity: 0.3;
+}
+
+.btn-default:hover {
+    opacity: 0.9;
+}
+
+.text-gray-100 {
+    color: #aeaeae !important;
+}
+
+::placeholder {
+    color: #aeaeae !important;
+}
+
+.bg-default {
+    background-color: #fff !important;
+}
+
+.line-center::before {
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: 0;
+    transform: translateY(50%);
+    width: 100%;
+    height: 0.3px;
+    background-color: hsla(0, 0%, 68%, 1);
+    z-index: -999;
+}
+
+.mb-default {
+    margin-bottom: 5rem !important;
+}
+
+a {
+    text-decoration: none;
+}
+
+a:hover {
+    text-decoration: underline;
+}
+
+.mt-default {
+    margin-top: 2.5rem !important;
+}
+
+.hero-images {
+    width: 100%;
+    height: 100%;
+    object-fit: cover !important;
+}
+
+/* custom scrolbar */
+::-webkit-scrollbar {
+    width: 14px;
+}
+::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 3px rgba(0 0 0 / 0.15);
+}
+::-webkit-scrollbar-thumb {
+    background: linear-gradient(125deg, #333, #293462);
+    border-radius: 10px;
+}
+
+.text-down {
+    width: 350px;
+    /* border: 1px solid red; */
+}
 </style>
