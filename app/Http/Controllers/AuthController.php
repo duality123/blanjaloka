482373e4e48192bbf6ebded8b59e9c222339745c
MUHAMMAD SAMBA <30502107+duality123@users.noreply.github.com>
2022-09-19 19:25:45 +0700

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Exception;
 use Illuminate\Http\Request;
-use App\Models\verify_user;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
