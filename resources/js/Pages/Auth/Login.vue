0a22202d693865bb59d8d6089eac3cfaad844bf3
MUHAMMAD SAMBA <30502107+duality123@users.noreply.github.com>
2022-09-21 21:03:12 +0700

    Update Login.vue

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -8,14 +8,14 @@
                 <form @submit.prevent="handleSubmit">
                     <div class="mb-3">
                         <label for="email" class="form-label">Email</label>
-                        <input type="email" v-model="form.email" class="form-control" id="email"
-                            placeholder="Masukkan email Anda">
+                        <input type="email" v-model="form.email" @keyup="handleDisableButton" class="form-control"
+                            id="email" placeholder="Masukkan email Anda">
                         <small class="text-danger" v-if="form.errors.email">{{ form.errors.email }}</small>
                     </div>
                     <div class="mb-3 position-relative">
                         <label for="kata_sandi" class="form-label">Kata Sandi</label>
-                        <input :type="passwordInputType" v-model="form.password" class="form-control" id="kata_sandi"
-                            placeholder="Masukkan kata sandi Anda">
+                        <input :type="passwordInputType" v-model="form.password" @keyup="handleDisableButton"
+                            class="form-control" id="kata_sandi" placeholder="Masukkan kata sandi Anda">
                         <small class="text-danger" v-if="form.errors.password">{{ form.errors.password }}</small>
                         <font-awesome-icon @click="handleTogglePassword" :icon="passwordEyeType"
                             class="position-absolute icon_eye" />
@@ -30,7 +30,8 @@
                         <a href="#" class="text-primary text-decoration-none">Lupa kata sandi?</a>
                     </div>
                     <div class="d-grid mt-4">
-                        <button type="submit" class="btn btn-primary text-white py-2">Masuk</button>
+                        <button type="submit" :disabled="isButtonDisable"
+                            class="btn btn-primary text-white py-2">Masuk</button>
                     </div>
                     <div class="d-flex justify-content-center mt-2 gap-1">
                         <p>Belum punya akun?</p>
@@ -60,26 +61,29 @@
 import AuthLayout from '../../Layouts/Auth.vue'
 import { ref } from 'vue'
 import { useForm } from '@inertiajs/inertia-vue3'
-
 const isPasswordVisible = ref(false);
+const isButtonDisable = ref(true);
 const passwordInputType = ref('password');
 const passwordEyeType = ref('fa-solid fa-eye-slash');
-
 const form = useForm({
     email: null,
     password: null,
     remember: false,
 });
-
 const handleTogglePassword = (e) => {
     isPasswordVisible.value = !isPasswordVisible.value;
     passwordInputType.value = isPasswordVisible.value ? 'text' : 'password';
     passwordEyeType.value = isPasswordVisible.value ? 'fa-solid fa-eye' : 'fa-solid fa-eye-slash';
 }
-
 const handleSubmit = () => {
     form.post('/login');
-    console.log(form);
+}
+const handleDisableButton = () => {
+    if ((form.email != null && form.password != null) && (form.email != '' && form.password != '')) {
+        isButtonDisable.value = false;
+    } else {
+        isButtonDisable.value = true;
+    }
 }
 </script>
 
@@ -89,40 +93,33 @@ h1 {
     font-weight: 600;
     margin-top: 3rem;
 }
-
 p {
     font-size: 16px;
     font-weight: 400;
     margin-bottom: 0;
 }
-
 form {
     margin-top: 1.5rem;
 }
-
 .form-label {
     font-size: 16px;
     font-weight: 600;
     margin-bottom: 0;
 }
-
 .form-control {
     border: none;
     border-radius: 0;
     border-bottom: 2px solid #AEAEAE;
     padding-left: 0;
 }
-
 .form-control:focus {
     box-shadow: none;
 }
-
 .icon_eye {
     bottom: 0.8rem;
     right: 0;
     cursor: pointer;
 }
-
 .oauth_choose {
     display: grid;
     grid-template-columns: 1fr 1.5fr 1fr;
@@ -130,13 +127,11 @@ form {
     justify-items: center;
     column-gap: 0.5rem;
 }
-
 .line {
     width: 100%;
     height: 2px;
     background-color: #AEAEAE;
 }
-
 @media (max-width: 575.98px) {
     .oauth_choose {
         grid-template-columns: 1fr 2fr 1fr;
