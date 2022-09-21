e6dda867713ba4e45f791f0efc49443f6d194bb5
rickydamarsaputra <retrocode.rc@gmail.com>
2022-09-21 19:01:48 +0700

    refactor code login page

---
+++ resources/js/Layouts/Auth.vue
@@ -0,0 +1,40 @@
+<template>
+
+  <Head>
+    <title>Blanjaloka - {{ title }}</title>
+    <meta name="description" content="Your page description">
+  </Head>
+  <div class="container-fluid">
+    <div class="row min-vh-100">
+      <div class="col-lg left_content"></div>
+      <div class="col-lg right_content">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { Head } from '@inertiajs/inertia-vue3'
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: 'please set a title props'
+  }
+});
+</script>
+
+<style scoped>
+.left_content {
+  background-image: url('../assets/images/auth_left_img.png');
+  background-position: center;
+  background-size: cover;
+}
+
+@media (max-width: 575.98px) {
+  .left_content {
+    display: none;
+  }
+}
+</style>
