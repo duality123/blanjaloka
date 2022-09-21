dda7a14fc5237cafd80c1e3f31b97d7071108663
MUHAMMAD SAMBA <30502107+duality123@users.noreply.github.com>
2022-09-21 18:27:27 +0700

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,10 +16,10 @@ class AuthController extends Controller
     public function providerCallback($provider)
     {
         $provider_user = Socialite::driver($provider)->user();
-        $find_user = User::where('email',$provider_user->email).first()
+        $find_user = User::where('email',$provider_user->email)->first();
         if($find_user){
-             $this->loginAndRedirect($provider_user);
-             return
+            Auth::login($find_user);
+            return redirect()->intended('/');
         }
         $user = User::updateOrCreate([
             'provider_id' => $provider_user->id,
@@ -32,14 +32,9 @@ class AuthController extends Controller
             'provider_token' =>$provider_user->token
         ]);
 
-        $this->loginAndRedirect($provider_user);
-        return
-    }
-    public function loginAndRedirect($user){
         Auth::login($user);
         return redirect()->intended('/');
     }
-
     public function redirectToProvider($provider)
     {
         return Socialite::driver($provider)->redirect();
