f0a6b3e065f08157e58e8eadecd64744fdf912ef
rickydamarsaputra <retrocode.rc@gmail.com>
2022-09-22 12:34:35 +0700

    setup blanjaloka color schama di scss

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -3,44 +3,44 @@
         <div class="row justify-content-center my-5">
             <div class="col-lg-8">
                 <img src="../../assets/images/blanjaloka_logo.png" alt="blanjaloka logo">
-                <h1>Masuk</h1>
-                <p class="text-secondary">Ayo lakukan pendanaan UMKM sekarang juga!</p>
+                <h1 class="text-neutral-black">Masuk</h1>
+                <p class="text-neutral-gray-4">Ayo lakukan pendanaan UMKM sekarang juga!</p>
                 <form @submit.prevent="handleSubmit">
                     <div class="mb-3">
-                        <label for="email" class="form-label">Email</label>
+                        <label for="email" class="form-label text-neutral-gray-5">Email</label>
                         <input type="email" v-model="form.email" @keyup="handleDisableButton" class="form-control"
                             id="email" placeholder="Masukkan email Anda">
                         <small class="text-danger" v-if="form.errors.email">{{ form.errors.email }}</small>
                     </div>
                     <div class="mb-3 position-relative">
-                        <label for="kata_sandi" class="form-label">Kata Sandi</label>
+                        <label for="kata_sandi" class="form-label text-neutral-gray-5">Kata Sandi</label>
                         <input :type="passwordInputType" v-model="form.password" @keyup="handleDisableButton"
                             class="form-control" id="kata_sandi" placeholder="Masukkan kata sandi Anda">
                         <small class="text-danger" v-if="form.errors.password">{{ form.errors.password }}</small>
                         <font-awesome-icon @click="handleTogglePassword" :icon="passwordEyeType"
-                            class="position-absolute icon_eye" />
+                            class="position-absolute text-primary-blue-6 icon_eye" />
                     </div>
                     <div class="d-flex justify-content-between align-items-center">
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" v-model="form.remember" id="ingat_saya">
-                            <label class="form-check-label" for="ingat_saya">
+                            <label class="form-check-label text-neutral-gray-4" for="ingat_saya">
                                 Ingat Saya
                             </label>
                         </div>
-                        <a href="#" class="text-primary text-decoration-none">Lupa kata sandi?</a>
+                        <a href="#" class="text-primary-blue-6 text-decoration-none">Lupa kata sandi?</a>
                     </div>
                     <div class="d-grid mt-4">
                         <button type="submit" :disabled="isButtonDisable"
-                            class="btn btn-primary text-white py-2">Masuk</button>
+                            class="btn btn-primary-blue-5 text-neutral-white py-2">Masuk</button>
                     </div>
                     <div class="d-flex justify-content-center mt-2 gap-1">
-                        <p>Belum punya akun?</p>
-                        <a href="#" class="text-primary text-decoration-none">Daftar sekarang</a>
+                        <p class="text-neutral-gray-4">Belum punya akun?</p>
+                        <a href="#" class="text-primary-blue-6 text-decoration-none">Daftar sekarang</a>
                     </div>
                 </form>
                 <div class="oauth_choose mt-4">
                     <div class="line"></div>
-                    <p>atau masuk dengan</p>
+                    <p class="text-neutral-gray-3">atau masuk dengan</p>
                     <div class="line"></div>
                 </div>
                 <div class="d-flex justify-content-center gap-4 mt-4">
