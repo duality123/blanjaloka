0aa0eda355f46d6951e81b76d6256e81e14ec6a2
Samba Alfaraby <sambaalfarabi@yahoo.com>
2022-11-14 11:08:20 +0700

    'build'

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.min.js"></script>
   @inertiaHead
+  @vite('resources/css/app.scss')
 </head>
 
 <body>
